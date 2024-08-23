e399092e309c3f6e6ffe0a481be60cd4314c09a4
shahzaib1044 <127635002+shahzaib1044@users.noreply.github.com>
2024-08-23 17:45:13 +0500

    Update index.js

--- sections/index.js
+++ sections/index.js
@@ -1,9 +1,9 @@
 import Hero from "./Hero"
-import Salon from "./Salon"
+
 import ShowCase from "./ShowCase"
 import Staff from "./Staff"
 import Blog from "./Blog"
 import Contact from "./Contact"
 import Appointment from "./Appointment"
 
-export { Hero, Salon, Staff, ShowCase, Blog, Contact,Appointment }
+export { Hero,  Staff, ShowCase, Blog, Contact,Appointment }
